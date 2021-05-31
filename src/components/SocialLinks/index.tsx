f6286089fbbdfc4bdfd5f22ea98309f3e394a99a
Arthur <maskalenkasth@gmail.com>
2021-05-31 16:00:21 -0300

    isso deve gerar o socialLinks

---
+++ src/components/SocialLinks/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './styles'
+
+import { Facebook, Instagram, Linkedin } from '@styled-icons/entypo-social'
+
+const SocialLinks = () => (
+  <S.Wrapper>
+    <S.IconWrapper>
+      <Facebook />
+    </S.IconWrapper>
+  </S.Wrapper>
+)
+
+export default SocialLinks
---
+++ src/components/SocialLinks/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div``
+
+export const IconWrapper = styled.div`
+  width: 3.4rem;
+  height: 3.4rem;
+  cursor: pointer;
+
+  &:hover {
+    color: red;
+    transition: 0.2s;
+  }
+`
