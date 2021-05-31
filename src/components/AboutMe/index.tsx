e971699cd0e03ddc2fbb00965e08a47ec0d01209
Arthur <maskalenkasth@gmail.com>
2021-05-31 05:50:28 -0300

    isso deve escrever mais testes

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -7,23 +7,20 @@ export type DataProps = {
 }
 
 export type AboutMeProps = {
-  image: string
-  data: DataProps[]
+  image?: string
+  data?: DataProps[]
 }
 
 const AboutMe = ({ image, data }: AboutMeProps) => (
   <S.Wrapper>
     {/* Photo init */}
-    <S.Photo src={image} />
+    <S.Photo src={image} alt="imagem de perfil" />
     {/* Photo end */}
 
     {/* Biohraphy init */}
     <S.Biography>
-      {/* Heading */}
       <Heading asTextType="title">Arthur Maskalenkas</Heading>
-      {/* SubHeading */}
       <Heading asTextType="subtitle">Web Developer</Heading>
-      {/* Texto */}
       <Heading asTextType="text">
         Prazer, meu nome é Arthur Maskalenkas. Sou um Web Developer com 7 meses
         de aprendizados constantes. Tenho bastante conhecimento em HTML5, CSS,
@@ -32,14 +29,16 @@ const AboutMe = ({ image, data }: AboutMeProps) => (
         vontade.
       </Heading>
       {/* div colunas age... */}
-      <S.Data>
-        {data.map((d) => (
-          <S.Column key={d.coluna}>
+
+      <S.Data aria-label="Varias colunas com informações sobre o Arthur">
+        {data!.map((d) => (
+          <S.Column key={d.coluna} className="coluna">
             <Heading asTextType="subtitle">{d.coluna}</Heading>
             <Heading asTextType="text">{d.data}</Heading>
           </S.Column>
         ))}
       </S.Data>
+
       {/* Social links */}
     </S.Biography>
     {/* Biohraphy end */}
--- src/components/AboutMe/test.tsx
+++ src/components/AboutMe/test.tsx
@@ -1,15 +1,28 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import AboutMe from '.'
+import mock from './mock'
 
 describe('<AboutMe />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<AboutMe />)
+  it('should render the About-Me', () => {
+    const { container, debug } = renderWithTheme(<AboutMe data={mock} />)
 
     expect(
-      screen.getByRole('heading', { name: /AboutMe/i }),
+      screen.getByRole('heading', { name: 'Arthur Maskalenkas' }),
     ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByAltText(/imagem de perfil/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: 'Web Developer' }),
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByLabelText(/Varias colunas com informações sobre o Arthur/i),
+    ).toBeInTheDocument()
+
+    // Testando se de fato tem 6 colunas
+    expect(container.querySelectorAll('.coluna')).toHaveLength(6)
   })
 })
