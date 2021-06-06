2ded160a51dd663484c96140e4891832bdc56efa
Arthur <maskalenkasth@gmail.com>
2021-06-06 08:05:50 -0300

    isso deve adicionar varios componentes pre montados

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,46 @@
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
+import * as S from './styles'
+
+//https://www.saltycrane.com/cheat-sheets/typescript/react/latest/
+
+// interface AnchorHTMLAttributes<T> extends HTMLAttributes<T> {
+//   download?: any;
+//   href?: string;
+//   hrefLang?: string;
+//   media?: string;
+//    ............
+
+// Componente pode receber href, onclick...
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
+
+export type ButtonProps = {
+  size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
+  icon?: JSX.Element
+  minimal?: boolean
+  as?: React.ElementType
+} & ButtonTypes
+
+const Button = ({
+  children,
+  icon,
+  size = 'medium',
+  fullWidth = false,
+  minimal = false,
+  ...props
+}: ButtonProps) => (
+  <S.Wrapper
+    size={size}
+    fullWidth={fullWidth}
+    hasIcon={!!icon}
+    minimal={minimal}
+    {...props}
+  >
+    {icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+)
+
+export default Button
