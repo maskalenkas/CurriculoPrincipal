50bde15f01430af23ff6e0855af9540322b72170
Arthur <maskalenkasth@gmail.com>
2021-06-08 05:10:19 -0300

    fix

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,9 +1,9 @@
-import * as S from './styles';
+import * as S from './styles'
 
 export type HomeTemplateProps = {
-  title?: string;
-  description?: string;
-};
+  title?: string
+  description?: string
+}
 
 // Recebendo as props do NEXT
 const Home = ({ title, description }: HomeTemplateProps) => (
@@ -19,6 +19,6 @@ const Home = ({ title, description }: HomeTemplateProps) => (
       alt="Um desenvolvedor de frente para uma tela com código."
     />
   </S.Wrapper>
-);
+)
 
-export default Home;
+export default Home
