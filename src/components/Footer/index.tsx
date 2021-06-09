9d9f70de390140985d8ee2c642c24a6cbb987c76
Arthur <maskalenkasth@gmail.com>
2021-06-09 07:39:00 -0300

    isso deve implementar o footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,14 @@
-import * as S from './styles';
+import * as S from './styles'
+
+import Heading from 'components/Heading'
 
 const Footer = () => (
   <S.Wrapper>
-    <h1>Footer</h1>
+    <S.Text asTextType="text" color="white" weight="light" size="xxlarge">
+      Feito com muito, mas muito <S.IconCoffee /> e <S.IconLove /> por Arthur
+      Maskalenkas
+    </S.Text>
   </S.Wrapper>
-);
+)
 
-export default Footer;
+export default Footer
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,52 @@
-import styled from 'styled-components';
+import styled, { css, DefaultTheme } from 'styled-components'
 
-export const Wrapper = styled.main``;
+import { Coffee, Heart } from '@styled-icons/boxicons-solid'
+
+import * as HeadingStyles from 'components/Heading/styles'
+import { colorTypes, HeadingProps } from 'components/Heading'
+
+export const Wrapper = styled.footer`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.primary};
+    padding: ${theme.spacings.small};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    user-select: none;
+  `}
+`
+
+const IconBaseModifier = {
+  Default: (
+    theme: DefaultTheme,
+    color: colorTypes,
+    hoverColor: 'red' | 'black',
+  ) => css`
+    width: 3.1rem;
+    color: ${theme.colors[color]};
+    box-sizing: content-box;
+    margin-top: -0.5rem;
+    transition: 0.4s;
+
+    :hover {
+      color: ${hoverColor};
+      transition: 0.4s;
+    }
+  `,
+}
+
+export const IconCoffee = styled(Coffee)`
+  ${({ theme }) => css`
+    ${IconBaseModifier.Default(theme, 'white', 'black')}
+  `}
+`
+
+export const IconLove = styled(Heart)`
+  ${({ theme }) => css`
+    ${IconBaseModifier.Default(theme, 'white', 'red')}
+  `}
+`
+
+export const Text = styled(HeadingStyles.Wrapper)`
+  ${({ theme }) => css``}
+`
