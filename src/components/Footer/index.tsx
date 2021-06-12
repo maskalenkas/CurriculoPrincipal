c7dd59ef6c1427336d186d1d2c2a84f4d53e805b
Arthur <maskalenkasth@gmail.com>
2021-06-12 19:05:11 -0300

    isso deve refatorar os icones usados

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,34 @@
 import * as S from './styles'
 
 import Heading from 'components/Heading'
+import Icons from 'components/Icons'
+
+import { Coffee, Heart } from '@styled-icons/boxicons-solid'
 
 const Footer = () => (
   <S.Wrapper>
     <S.Text asTextType="text" color="white" weight="light" size="xxlarge">
-      Feito com muito, mas muito <S.IconCoffee /> e <S.IconLove /> por Arthur
-      Maskalenkas
+      Feito com muito, mas muito{' '}
+      <Icons
+        color="white"
+        size="3.1rem"
+        marginTop="-0.5rem"
+        hoverColor="black"
+        sizeMedia="2.5rem"
+      >
+        <Coffee />
+      </Icons>{' '}
+      e{' '}
+      <Icons
+        color="white"
+        size="3.1rem"
+        marginTop="-0.5rem"
+        hoverColor="red"
+        sizeMedia="2.5rem"
+      >
+        <Heart />
+      </Icons>{' '}
+      por Arthur Maskalenkas
     </S.Text>
   </S.Wrapper>
 )
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -3,14 +3,14 @@ import styled, { css, DefaultTheme } from 'styled-components'
 import { Coffee, Heart } from '@styled-icons/boxicons-solid'
 
 import * as HeadingStyles from 'components/Heading/styles'
-import { colorTypes, HeadingProps } from 'components/Heading'
 import media from 'styled-media-query'
 
 export const Wrapper = styled.footer`
   ${({ theme }) => css`
-    background-color: ${theme.colors.primary};
     padding: ${theme.spacings.small};
+    background-color: ${theme.colors.primary};
     display: flex;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
     user-select: none;
@@ -18,43 +18,6 @@ export const Wrapper = styled.footer`
     ${media.lessThan('medium')`
       padding: ${theme.spacings.xsmall};
     `}
-  `}
-`
-
-// Molde dos icons componentes
-const IconBaseModifier = {
-  Default: (
-    theme: DefaultTheme,
-    color: colorTypes,
-    hoverColor: 'red' | 'black',
-  ) => css`
-    width: 3.1rem;
-    color: ${theme.colors[color]};
-    box-sizing: content-box;
-    margin-top: -0.5rem;
-    transition: 0.4s;
-
-    :hover {
-      color: ${hoverColor};
-      transition: 0.4s;
-    }
-
-    ${media.lessThan('medium')`
-      width: 2.5rem;
-    `}
-  `,
-}
-
-// Componentes
-export const IconCoffee = styled(Coffee)`
-  ${({ theme }) => css`
-    ${IconBaseModifier.Default(theme, 'white', 'black')}
-  `}
-`
-
-export const IconLove = styled(Heart)`
-  ${({ theme }) => css`
-    ${IconBaseModifier.Default(theme, 'white', 'red')}
   `}
 `
 
