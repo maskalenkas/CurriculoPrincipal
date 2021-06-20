c62a5902b8a378884cfbe75a7af32e6f64cfb98b
Arthur <maskalenkasth@gmail.com>
2021-06-20 20:41:45 -0300

    isso deve testar 100% skills

--- src/components/Skills/styles.ts
+++ src/components/Skills/styles.ts
@@ -9,43 +9,6 @@ import { Wrapper as HeadingStyle } from 'components/Heading/styles'
 import { Testinglibrary, Cypress } from '@styled-icons/simple-icons'
 import media from 'styled-media-query'
 
-export const Wrapper = styled.main`
-  ${({ theme }) => css`
-    position: relative;
-    padding: 6.5rem 3rem 3rem 3rem;
-    background: ${theme.colors.white};
-
-    ${media.lessThan('medium')`
-      padding: 4rem 2rem 4rem 2rem;
-    `}
-  `}
-`
-
-export const Title = styled.div`
-  ${({ theme }) => css`
-    user-select: none;
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    top: -3.5rem;
-    padding: 1.8rem 1.7rem;
-    background-color: ${theme.colors.primary};
-
-    ${HeadingStyle} {
-      text-align: center;
-    }
-
-    ${media.lessThan('medium')`
-      padding: 1.5rem 1.5rem;
-      top: -2.8rem;
-      
-      ${HeadingStyle} {
-        font-size: ${theme.font.sizes.xlarge}
-      }
-    `}
-  `}
-`
-
 export const Content = styled.div`
   ${({ theme }) => css`
     /* Fazendo com que tenha coluna de textos e icones */
