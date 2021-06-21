5271d2ba40a2d7c60ab26b20ee90eba24a16473d
Arthur <maskalenkasth@gmail.com>
2021-06-21 12:28:55 -0300

    fix2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,3 +3,14 @@ import Home from 'templates/Home'
 export default function Index() {
   return <Home />
 }
+
+export function getStaticProps() {
+  // faz lógica como:
+  // pode buscar dados numa API
+  // fazer calculo|leitura de context
+
+  // retorno dos dados
+  return {
+    props: {},
+  }
+}
