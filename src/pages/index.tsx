8ee6194164932299cc4e011dcd9d4b0e1cf28228
Arthur <maskalenkasth@gmail.com>
2021-06-20 23:32:27 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,5 @@
-import Home, { HomeTemplateProps } from 'templates/Home'
-export default function Index(props: HomeTemplateProps) {
-  return <Home {...props} />
-}
+import Home from 'templates/Home'
 
-export function getStaticProps() {
-  // faz lógica como:
-  // pode buscar dados numa API
-  // fazer calculo|leitura de context
-
-  // retorno dos dados
-  return {
-    props: {
-      title: 'React avançado',
-      description: 'TypeScript, ReactJS, NextJS e Styled Components',
-    },
-  }
+export default function Index() {
+  return <Home />
 }
